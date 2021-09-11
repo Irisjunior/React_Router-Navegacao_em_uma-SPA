c8faf267cec25d69ef08a7a199a4679d61c2d630
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-09-11 18:26:23 -0300

    iniciando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./assets/css/base/base.css";
+import Home from "./paginas/Home";
+import Sobre from "./paginas/Sobre";
+
+function App() {
+  const router = () => {
+    const location = window.location.pathname;
+    if (location === "/sobre") {
+      return <Sobre />;
+    } else {
+      return <Home />;
+    }
+  };
+  return <>{router()}</>;
+}
+
+export default App;
